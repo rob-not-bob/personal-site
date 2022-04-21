fdd600829f1253d960970625c93d754de527b691
Robert Allen <robert.allen@viasat.com>
2022-04-21 14:36:13 -0700

    Reinit with resume

---
+++ apps/personal-site/src/app/app.tsx
@@ -0,0 +1,49 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <>
+      {/* START: routes */}
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+          <li>
+            <a href="/assets/resume.pdf" target="_blank">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            This is the generated root route.{' '}
+            <Link to="/page-2">Click here for page 2.</Link>
+          </div>
+        )}
+      />
+      <Route
+        path="/page-2"
+        exact
+        render={() => (
+          <div>
+            <Link to="/">Click here to go back to root page.</Link>
+          </div>
+        )}
+      />
+      {/* END: routes */}
+    </>
+  );
+}
+
+export default App;
