4e244fec8cf8d13098b9f8bf848b7644ab6c1455
Robert Allen <robert.allen@viasat.com>
2023-07-23 10:06:46 -0600

    Convert posts to work with new Img tag

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -2,6 +2,7 @@ import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
 const config = defineConfig({
   extensions: ['.svelte.md', '.md', '.svx'],
+  layout: './src/routes/components/defaultPostLayout.svelte',
 
   smartypants: {
     dashes: 'oldschool'
